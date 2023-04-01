9bbef0ac088a6dd8b78e4d4cc381e7d7c907077c
JAghazada <=>
2023-04-02 01:05:16 +0400

    corsoption deleted

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const dotenv = require("dotenv");
 const router = require("./routers");
 const session = require("express-session");
 const cors = require("cors");
-const corsOption = require("./helpers/corsOptions")
 const mongoSanitize = require("express-mongo-sanitize")
 const errorHandlerMiddleware = require("./middlewares/ErrorHandler");
 const bodyParser = require("body-parser");
@@ -15,7 +14,7 @@ const MongoStore = require("connect-mongo");
 dotenv.config();
 const port = process.env.PORT || 5001;
 //!Middlewares
-app.use(cors(corsOption))
+app.use(cors())
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text({type: '/'}));
