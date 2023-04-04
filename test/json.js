25df12a28485d647f4225b34a8b955eb32ec5132
JAghazada <=>
2023-04-05 03:35:31 +0400

    test/json.js updated for stupid reason

--- test/json.js
+++
@@ -1,8 +0,0 @@
-const today = new Date();
-const day = today.getDate();
-const month = today.getMonth() + 1;
-const year = today.getFullYear();
-
-const formattedDate = `${day < 10 ? '0' + day : day}.${month < 10 ? '0' + month : month}.${year}`;
-
-console.log(formattedDate);
