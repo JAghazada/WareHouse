144897e6c16c4193d0b3519f1fd3411269a03529
JAghazada <jamil.aghazada03@gmail.com>
2023-04-13 16:52:26 +0400

    files removed and views created

--- routers/productInfo.routes.js
+++
@@ -1,5 +0,0 @@
-const productInfoController = require("../controllers/productInfo.controller");
-
-const router = require("express").Router();
-router.post("/productInfo",productInfoController)
-module.exports = router
--- views/public/js/bill/profit-bill.js
+++
@@ -1,9 +0,0 @@
-const DateInput = document.querySelector(".pickupdt>input");
-const SearchButton = document.querySelector(".search-bill-btn");
-const today = new Date().toISOString().substr(0, 10);
-DateInput.value = today;
-
-SearchButton.addEventListener("click", () => {
-    const date = DateInput.value;
-    window.location.href = `/products/bill/profits?date=${date}`
-});
