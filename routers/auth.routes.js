5d4969c9d326b8111ea6b0a1af2045f5a7e5dc2a
JAghazada <jamil.aghazada03@gmail.com>
2023-06-30 17:17:59 +0400

    C:/Program Files/Git/ redirect added

--- routers/auth.routes.js
+++ routers/auth.routes.js
@@ -2,17 +2,17 @@ const { login, register, logout } = require("../controllers/auth.controller")
 const { checkToken } = require("../helpers/auth/checkToken")
 const AuthValidation = require("../middlewares/validation/auth.validation")
 const router = require("express").Router()
-router.get("/",(req,res)=>{
-    res.render("login")
+router.get("/", (req, res) => {
+    res.redirect("login")
 })
-router.get("/login",checkToken,(req,res)=>{
+router.get("/login", checkToken, (req, res) => {
     res.render("login");
 });
-router.get("/register",checkToken,(req,res)=>{
+router.get("/register", checkToken, (req, res) => {
     res.render("register");
 });
-router.get("/logout",logout);
-router.post("/login",AuthValidation.login, login)
+router.get("/logout", logout);
+router.post("/login", AuthValidation.login, login)
 router.post("/register", AuthValidation.register, register)
 // router.post("/me",tokenCheck,me)
 module.exports = router
