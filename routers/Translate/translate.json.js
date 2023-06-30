7ec1579750e5e1f82b2a8786fdaa5703129fb17a
JAghazada <jamil.aghazada03@gmail.com>
2023-06-30 16:01:07 +0400

    Replace main branch with dev branch

---
+++ routers/Translate/translate.json.js
@@ -0,0 +1,16 @@
+const translateJson = {
+    number: "Əd",
+    kg: "Kg",
+    cm: "Sm",
+    dm: "Dm",
+    box: "Qutu",
+    meter: "Metr",
+    block: "Blok",
+    wholesale: "Top",
+    bag: "Kisə",
+    set: "Dəst",
+    packet: "Paket",
+    add:"Əlavə edilmə",
+    create:"Anbara yazılma"
+  };
+module.exports = translateJson
